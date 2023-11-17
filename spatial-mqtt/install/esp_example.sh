ec66f45e060dd9771942898a2ecbd8b1869e13e2
Donald-vdW <113445769+Donald-vdW@users.noreply.github.com>
2023-11-17 08:28:27 +0200

    Delete spatial-mqtt/install/esp_example.sh

--- spatial-mqtt/install/esp_example.sh
+++
@@ -1,6 +0,0 @@
-task_esp_example(){
-    mkdir -p $HOME/esp/test
-    cd $HOME/esp/test
-    cp -r $HOME/esp/esp-idf/examples/protocols/mqtt/tcp $HOME/esp/esp-idf/examples/get-started/* $HOME/esp/test/
-    # sudo chmod a+rw /dev/ttyUSB1
-}
