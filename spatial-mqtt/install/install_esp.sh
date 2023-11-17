ceec8fd698c36e8a970fbbafe0fe7a1b94d37102
Donald-vdW <113445769+Donald-vdW@users.noreply.github.com>
2023-11-17 08:28:41 +0200

    Delete spatial-mqtt/install/install_esp.sh

--- spatial-mqtt/install/install_esp.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Create esp directory and install ESP-IDF
-cd $HOME/esp
-git clone --recursive http://github.com/espressif/esp-idf.git
-cd $HOME/esp/esp-idf
-/usr/bin/python3 $HOME/esp/esp-idf/tools/idf_tools.py install
-$HOME/esp/esp-idf/tools/idf_tools.py install-python-env
-./install.sh esp32,esp32c3,esp32s3
